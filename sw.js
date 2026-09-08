ecafe30dd088ce38753c9d9362a0d251337479f2
thisisnotdesiree-prog <thisisnotdesiree@gmail.com>
2026-09-08 14:05:17 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+```javascript
+const CACHE_NAME = "penny-counter-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function (name) {
+                        return name !== CACHE_NAME;
+                    })
+                    .map(function (name) {
+                        return caches.delete(name);
+                    })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+```
